838a7b7e577d8ccd06299b5e9bd7f6fd844a9d2b
sahil <sahildhimansanour686@gmail.com>
2019-09-21 10:30:09 +0530

    added the modal and formik form

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from 'react-responsive-modal'
+import { LoginUserForm } from './'
 
 const Login = () => {
+    const [modalOpen, setModalOpen] = useState(false)
+    
+    const onOpenModal = () => {
+        setModalOpen(true)
+    };
+    const onCloseModal = () => {
+        setModalOpen(false)
+    };
+
     return(
         <div>
             <h1>Login Form</h1>
+            <button onClick={onOpenModal}>Open modal</button>
+            <Modal open={modalOpen} onClose={onCloseModal} center>
+            <h2>Simple centered modal</h2>
+                <LoginUserForm />
+            </Modal>            
         </div>
     )
 }
--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,1 +1,2 @@
 export { default as LoginUser } from './Login'
+export { default as LoginUserForm } from './LoginForm'
